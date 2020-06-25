d82ab644bde02d52603fdb45bdfc459e3797dbfd
Mortuz <mortuzalam@gmail.com>
2020-06-25 18:32:58 +0530

    add slider

---
+++ js/components/navbar.js
@@ -0,0 +1,47 @@
+document.write(`
+<nav class="navbar navbar-expand-lg navbar-light">
+      <div class="container">
+        <a class="navbar-brand" href="#">
+          <img src="./assets/img/logo.png" alt="ImmobilApp" height="30" />
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#mainNav"
+          aria-controls="mainNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="mainNav">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Proproetari</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Agenzie</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Pricing</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Chi siamo</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Blog</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contatti</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="btn btn-primary" href="#">Accedi</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+`)
