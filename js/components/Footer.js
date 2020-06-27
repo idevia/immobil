f0146811fcb16757163ba5e41f15e97a87b1f386
SternritterFarid <relampagolanzadel04@gmail.com>
2020-06-27 16:14:36 +0530

    Added web components and modified SASS structure.

---
+++ js/components/Footer.js
@@ -0,0 +1,72 @@
+const footerTemplate = /*html*/ `
+<div class="footer">
+<div class="container">
+  <div class="footer__logo">
+    <img src="./assets/img/logo.png" alt="ImmobilApp" height="30" />
+  </div>
+  <div class="row py-4">
+    <div class="col-md-3 col-sm-12">
+      <h6 class="font-weight-bold">Contact Us</h6>
+      <div class="footer__list">
+        <ul>
+          <li>+(290) 520-7927</li>
+          <li>Bangalore India</li>
+          <li>idevia.in</li>
+        </ul>
+      </div>
+    </div>
+    <div class="col-md-3 col-sm-12">
+      <h6 class="font-weight-bold">Our Company</h6>
+      <div class="footer__list">
+        <ul>
+          <li>About</li>
+          <li>Product</li>
+          <li>Portfolio</li>
+          <li>Career</li>
+          <li>Blog</li>
+        </ul>
+      </div>
+    </div>
+    <div class="col-md-3 col-sm-12">
+      <h6 class="font-weight-bold">Social Contacts</h6>
+      <div class="footer__list">
+        <ul>
+          <li>Facebook</li>
+          <li>Linked in</li>
+          <li>Youtube</li>
+          <li>Vimeo</li>
+          <li>Skype</li>
+        </ul>
+      </div>
+    </div>
+    <div class="col-md-3 col-sm-12">
+      <h6 class="font-weight-bold">Address</h6>
+      <div class="footer__list">
+        <ul>
+          <li>
+            Idevia Tech Private Limited<br />
+            Kolkata, India
+          </li>
+          <li>
+            <h6 class="font-weight-bold">Follow us</h6>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="footer__copyright">
+  <p>Copyright@ReSmart 2020</p>
+</div>
+</div>
+`
+
+class Footer extends HTMLElement {
+  constructor() {
+    super()
+    this.innerHTML = footerTemplate
+  }
+}
+
+window.customElements.define("site-footer", Footer)
--- js/components/navbar.js
+++ js/components/navbar.js
@@ -1,47 +1,55 @@
-document.write(`
-<nav class="navbar navbar-expand-lg navbar-light">
-      <div class="container">
-        <a class="navbar-brand" href="#">
-          <img src="./assets/img/logo.png" alt="ImmobilApp" height="30" />
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#mainNav"
-          aria-controls="mainNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
+const navbarTemplate = /*html*/ `
+<nav class="navbar navbar-expand-lg navbar-light mt-5">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <img src="./assets/img/logo.png" alt="ImmobilApp" height="30" />
+    </a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#mainNav"
+      aria-controls="mainNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-        <div class="collapse navbar-collapse" id="mainNav">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="#">Proproetari</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Agenzie</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Pricing</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Chi siamo</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Blog</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Contatti</a>
-            </li>
+    <div class="collapse navbar-collapse" id="mainNav">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Proprietari</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Agenzie</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Chi siamo</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Blog</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contatti</a>
+        </li>
 
-            <li class="nav-item">
-              <a class="btn btn-primary" href="#">Accedi</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-`)
+        <li class="nav-item ml-4">
+          <a class="btn btn-primary" href="#">Accedi</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>`
+
+class Navbar extends HTMLElement {
+  constructor() {
+    super()
+    this.innerHTML = navbarTemplate
+  }
+}
+
+window.customElements.define("nav-bar", Navbar)
