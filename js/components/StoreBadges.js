e26e819a15978de53a81d80f3d58f682c26becbd
Mortuz Alam <mortuz@Mortuzs-Mac-mini.local>
2020-07-14 13:19:13 +0530

    add store badges

---
+++ js/components/StoreBadges.js
@@ -0,0 +1,7 @@
+document.write(`
+  <p class="text-center">Coming soon</p> 
+  <div class="d-flex justify-content-center store-badges">
+    <a href="#!"><img src="/assets/img/store/app-store.png" /></a>
+    <a href="#!"><img src="/assets/img/store/play-store.png" /></a>
+  </div>  
+`)
